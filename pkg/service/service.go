a4ec9216b55a8a35e46514144b7ccd42f9876f34
Yury Tomilov <tomilov_yur@mail.ru>
2022-11-21 22:24:41 +0300

    15

--- pkg/service/service.go
+++ pkg/service/service.go
@@ -5,8 +5,6 @@ import (
 	"github.com/lenx/todo/pkg/repository"
 )
 
-//go:generate mockgen -source=service.go -destination=mocks/mock.go
-
 type Authorization interface {
 	CreateUser(user todo.User) (int, error)
 	GenerateToken(username, password string) (string, error)
